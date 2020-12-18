04e4b919c95cd1198469db6873faa45351bc5b4f
Mandeep2310 <71230920+Mandeep2310@users.noreply.github.com>
2020-12-17 19:41:34 -0500

    CREATING THE TAB LAYOUT

---
+++ MandeepFinalD2/src/main/java/mandeep/singh/n01230342/MandeepActivity.java
@@ -0,0 +1,14 @@
+package mandeep.singh.n01230342;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MandeepActivity extends AppCompatActivity {
+    //Mandeep Singh, N01230342, Section B
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mandeep);
+    }
+}
